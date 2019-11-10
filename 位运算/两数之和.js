18fed4684a53ae555393225bd4edc2e5e7df0ee6
Mwangzhi <519319609@qq.com>
2019-11-10 22:31:14 +0800

    add bit operation

--- 位运算/两数之和.js
+++ 位运算/两数之和.js
@@ -1,14 +1,15 @@
 
 
 
-
 //位运算加法
+//方法一
 function bitAdd(m, n) {
     while (m) {
         [m, n] = [(m & n) << 1, m ^ n];
     }
     return n;
 }
+//方法二
 var getSum = function (a, b) {
     while (b != 0) {
         let c = a & b;
