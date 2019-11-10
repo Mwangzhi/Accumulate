3d2283cc2b7e44909a52c86bd88987ad70bbc197
Mwangzhi <519319609@qq.com>
2019-11-10 22:30:15 +0800

    add bit operation

--- 位运算/两数之和.js
+++ 位运算/两数之和.js
@@ -9,4 +9,12 @@ function bitAdd(m, n) {
     }
     return n;
 }
+var getSum = function (a, b) {
+    while (b != 0) {
+        let c = a & b;
+        a = a ^ b;
+        b = c << 1;
+    }
+    return a;
+};
 console.log(bitAdd(22, 43))
