d40a01b4e3fca93953199de32e398005a30de8ac
Mwangzhi <519319609@qq.com>
2020-02-13 19:54:00 +0800

    JavaScript BT practice

---
+++ js变态题.js
@@ -0,0 +1,16 @@
+
+
+//demo1
+var a = 1.0 - 0.9;
+if (a == 0.1) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+var b = 0.8 - 0.7;
+if (a == b) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+// 答案:false false 0.9 0.8本身在二进制表示下已经失去精度。
